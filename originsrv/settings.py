a3521c2e538da0d949e0e9de0993411dd3646952
huoyijie <yijie.huo@foxmail.com>
2023-03-11 21:55:53 +0800

    init

--- originsrv/settings.py
+++ originsrv/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -111,7 +112,6 @@ TIME_ZONE = 'Asia/Shanghai'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- webapp/admin.py
+++ webapp/admin.py
@@ -4,11 +4,8 @@ from django.contrib import admin
 from webapp.models import Resource
 
 
-# class Site(admin.AdminSite):
-#     site_title = _('Origin server admin')
-
-#     # Text to put in each page's <h1>.
-#     site_header = _('Origin server administration')
+admin.site.site_title = _('Origin server admin')
+admin.site.site_header = _('Origin server administration')
 
 
 @admin.register(Resource)
