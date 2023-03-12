efaa7cc6910779bc8f8b492b3fe277fe3cb02dba
huoyijie <yijie.huo@foxmail.com>
2023-03-12 13:02:54 +0800

    update .gitignore

--- originsrv/settings.py
+++ originsrv/settings.py
@@ -118,9 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'public/static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = BASE_DIR / 'public'
+MEDIA_ROOT = BASE_DIR / 'public/media'
