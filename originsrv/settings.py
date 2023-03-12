2bac73e1eb859dafa37250b877e663c3e6337b3a
huoyijie <yijie.huo@foxmail.com>
2023-03-12 11:08:23 +0800

    update settings

--- originsrv/settings.py
+++ originsrv/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0+ax*w3)&i!%sgf*9rjti^90nd$&6r7xl(7f(r(cvohwwi-0dw'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -124,3 +124,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
--- webapp/models.py
+++ webapp/models.py
@@ -1,4 +1,4 @@
-from django.utils.translation import gettext, gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 
